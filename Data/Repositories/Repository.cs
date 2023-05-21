4c9f1f4f3c65a349be265c1370e868e9f98494d7
Cubigator <sergeychuprin2006@gmail.com>
2023-05-21 14:21:25 +0700

    refactoring and add generic repository

---
+++ Data/Repositories/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyBlog.Data.Repositories
+{
+    public class Repository<T> : IRepository<T>
+        where T : class
+    {
+        Context _context;
+        public Repository(Context context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(T model)
+        {
+            if (model is null)
+                return;
+
+            await _context.AddAsync(model);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T model)
+        {
+            if (model is null)
+                return;
+
+            _context.Remove(model);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GelAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _context.FindAsync<T>();
+        }
+
+        public async Task UpdateAsync(T model)
+        {
+            _context.Update(model);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
