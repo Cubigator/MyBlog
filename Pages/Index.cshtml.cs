b2dace048dfa1877187ee92ee321543fcf64f879
Cubigator <sergeychuprin2006@gmail.com>
2023-05-23 00:24:06 +0700

    get articles and add article card

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyBlog.Data.EntityModels;
+using MyBlog.Data.Repositories;
 
 namespace MyBlog.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private IRepository<Article> _articleRepository;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public List<Article> Articles { get; set; } = new List<Article>();
+
+        public IndexModel(ILogger<IndexModel> logger, IRepository<Article> repository)
         {
             _logger = logger;
+            _articleRepository = repository;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
-
+            Articles = (await _articleRepository.GelAllAsync()).ToList();
         }
     }
 }
