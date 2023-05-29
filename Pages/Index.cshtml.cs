19fa6c90ddb2d1911cd1a0a0d1916d6baac30848
Cubigator <sergeychuprin2006@gmail.com>
2023-05-29 14:35:20 +0700

    add entity view models

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using MyBlog.Data.EntityModels;
 using MyBlog.Data.Repositories;
+using MyBlog.Pages.ViewModels;
 
 namespace MyBlog.Pages
 {
@@ -10,7 +10,8 @@ namespace MyBlog.Pages
         private readonly ILogger<IndexModel> _logger;
         private IRepository<Article> _articleRepository;
 
-        public List<Article> Articles { get; set; } = new List<Article>();
+        public List<ArticleViewModel> Articles { get; set; } = new List<ArticleViewModel>();
+        public List<ContentBlock> Blocks { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger, IRepository<Article> repository)
         {
@@ -20,7 +21,17 @@ namespace MyBlog.Pages
 
         public async Task OnGet()
         {
-            Articles = (await _articleRepository.GelAllAsync()).ToList();
+            Articles = (await _articleRepository.GelAllAsync()).Select(item =>
+                new ArticleViewModel()
+                {
+                    Id = item.Id,
+                    Header = item.Header,
+                    Introduction = item.Introduction,
+                    Image = item.Image,
+                    CreationDate = item.CreationDate,
+                    LastModifiedDate = item.LastModifiedDate,
+                    ReadingTime = item.ReadingTime
+                }).ToList();
         }
     }
 }
