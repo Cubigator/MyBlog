9bfa1612c5d5e9dee9a61b76f0c3eae48545a1eb
Cubigator <sergeychuprin2006@gmail.com>
2023-06-10 02:26:18 +0700

    password encryptor

---
+++ Services/EncryptorService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Text;
+
+namespace MyBlog.Services;
+
+public class EncryptorService
+{
+    public string HashPassword(string password, string salt)
+    {
+        return Convert.ToBase64String(KeyDerivation.Pbkdf2
+                                        (
+                                        password, 
+                                        Encoding.Unicode.GetBytes(salt),
+                                        KeyDerivationPrf.HMACSHA512,
+                                        5000,
+                                        64
+                                        ));
+    }
+}
